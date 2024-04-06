e1e861774f416ee106ec3f2ec31442dd34ac8798
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2024-04-06 16:30:56 +0500

    Initial Commit

---
+++ TicTakToe/TicTakToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTakToeApp.swift
+//  TicTakToe
+//
+//  Created by Лилия Андреева on 06.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTakToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
